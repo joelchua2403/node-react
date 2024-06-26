d13b6cf6dd25486d905809416a4ad6bffc295a96
Joel <joel.chua.mingyi@gmail.com>
2024-06-26 14:13:58 +0800

    update usergroups

--- backend/routes/UserGroups.js
+++ backend/routes/UserGroups.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { Group, UserGroup, User } = require('../models');
 const verifyToken = require('../middleware/authMiddleware');
 const { sequelize } = require('../models');
+const { CheckGroup } = require('../middleware/groupAuthMiddleware');
 
 // Fetch all users with their groups
 
@@ -31,6 +32,36 @@ router.get('/', async (req, res) => {
   } catch (err) {
     console.log(err);
     res.status(500).json({ error: err });
+  }
+});
+
+// Get all groups for a user
+router.get('/:username/:app_acronym', async (req, res) => {
+  const { username, app_acronym } = req.params;
+console.log('username:', username);
+console.log('app_acronym:', app_acronym);
+  try {
+    const user = await User.findOne({
+      where: { username},
+      include: {
+        model: Group,
+        through: UserGroup,
+        as: 'groups',
+        attributes: ['name']
+      }
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    const isInGroupProjectManager = await CheckGroup(username, `${app_acronym}_Pm`);
+
+    res.json({
+      isInGroupProjectManager
+    });
+  } catch (error) {
+    console.error('Error fetching user groups:', error);
+    res.status(500).json({ error: 'Error fetching user groups' });
   }
 });
 
